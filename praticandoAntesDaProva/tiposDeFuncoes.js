ab3e160e6055ffbbfc9b727985b709a1b3876d31
felipeS <felipecsena877@gmail.com>
2023-10-29 13:18:28 -0300

    funcoesfeitas

--- praticandoAntesDaProva/tiposDeFuncoes.js
+++ praticandoAntesDaProva/tiposDeFuncoes.js
@@ -1,4 +1,5 @@
 
+//ex1
 function retornaPares () {
     let inteiros = [2, 3, 4, 5, 6, 9, 10]
 
@@ -6,7 +7,7 @@ function retornaPares () {
 
     for(let i = 0; i < inteiros.length; i++){
         if(inteiros[i] % 2 === 0){
-            pares.push(inteiros[i
+            pares.push(inteiros[i])
         }
     }
 
@@ -14,4 +15,33 @@ function retornaPares () {
 
 }
 
+//ex2
+let anonima = function () {
+    let inteiros = [2, 3, 4, 5, 6, 9, 10]
+
+    let impares = []
+
+    for(let i = 0; i < inteiros.length; i++){
+        if(inteiros[i] % 2 !== 0){
+            impares.push(inteiros[i])
+        }
+    }
+
+    console.log(impares)
+}
+
+//ex3
+function fatorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
+    }
+
+}
+
+
+//executando
 retornaPares()
+anonima()
+console.log(fatorial(5))

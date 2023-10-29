42f77cba230d64e76a91209030d041ece0d70714
felipeS <felipecsena877@gmail.com>
2023-10-29 12:56:34 -0300

    ex1

---
+++ praticandoAntesDaProva/tiposDeFuncoes.js
@@ -0,0 +1,17 @@
+
+function retornaPares () {
+    let inteiros = [2, 3, 4, 5, 6, 9, 10]
+
+    let pares = []
+
+    for(let i = 0; i < inteiros.length; i++){
+        if(inteiros[i] % 2 === 0){
+            pares.push(inteiros[i
+        }
+    }
+
+    console.log(pares)
+
+}
+
+retornaPares()

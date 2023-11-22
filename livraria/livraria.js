038fd3730c12d29f8fda7bc89f5e6f0ed545e53d
felipeS <felipecsena877@gmail.com>
2023-11-21 22:58:44 -0300

    finished

--- livraria/livraria.js
+++ livraria/livraria.js
@@ -1,33 +1,40 @@
-
 class EntidadeBibliografica {
     constructor(titulo, autor, anoPublicacao, codigo){
-        this.titulo = titulo
-        this.autor = autor
-        this.anoPublicacao = anoPublicacao
-        this.codigo = codigo
-        this.emprestado = false
-        this.usuarioEmprestimo = null
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.codigo = codigo;
+        this.emprestado = false;
+        this.usuarioEmprestimo = null;
+    }
+
+    emprestar(usuario) {
+        if (this.emprestado == true) {
+            console.log('este livro já está emprestado');
+        }
+        else {
+            this.emprestado = true;
+            this.usuarioEmprestimo = usuario;
+            console.log('livro emprestado com sucesso');
+        }
+    }
+
+    devolver() {
+        if (this.emprestado == false) {
+            console.log('este livro nao foi emprestado');
+        } else {
+            this.emprestado = false;
+            this.usuarioEmprestimo = null;
+            console.log('livro foi devolvido');
+        }
     }
 }
 
-emprestar () {
-    if (this.emprestado == true) {
-        console.log('este livro já está emprestado')
-    }
-    else {
-        this.emprestado = true
-        this.usuarioEmprestimo = usuario
-        console.log('livro emprestado com sucesso')
-    }
-}
-
-devolver () {
-    if (this.emprestado == false) {
-        console.log('este livro nao foi emprestado')
-    } else {
-        this.emprestado = false
-        this.usuarioEmprestimo = false
-        console.log('livro foi devolvido')
+class usuario {
+    constructor(nome, registroAcademico, dataNascimento){
+        this.nome = nome
+        this.registroAcademico = registroAcademico
+        this.dataNascimento = new Date(dataNascimento) //formata em formato de data
     }
 }
 
@@ -39,15 +46,15 @@ const generoLivro = {
 }
 
 class Livro extends EntidadeBibliografica {
-    constructor(titulo, autor, anoPublicacao, codigo){
-        super(titulo, autor, anoPublicacao, codigo)
-        this.genero = genero
+    constructor(titulo, autor, anoPublicacao, codigo, genero){
+        super(titulo, autor, anoPublicacao, codigo);
+        this.genero = genero;
     }
 }
 
 class Revista extends EntidadeBibliografica {
-    constructor(titulo, autor, anoPublicacao, codigo){
-        super(titulo, autor, anoPublicacao, codigo)
-        this.genero = genero
+    constructor(titulo, autor, anoPublicacao, codigo, genero){
+        super(titulo, autor, anoPublicacao, codigo);
+        this.genero = genero;
     }
 }
